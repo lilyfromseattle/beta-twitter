a0cd9b4dc2e1c2b6b7a5651d2c43a5aa037765c6
Lily Pace <lilyfpace@gmail.com>
2014-11-03 07:10:53 -0800

    stuff

---
+++ app/controllers/tweets_controller.rb
@@ -0,0 +1,24 @@
+class TweetsController < ApplicationController
+
+def index
+  @tweets = Tweet.all
+
+end
+
+def show
+  @tweet = Tweet.find(params[:id])
+end
+
+def new
+  @tweet = Tweet.new
+end
+
+def create
+  @tweet = Tweet.new(params.require(:tweet).permit(:author, :body ))
+  if @tweet.save
+    redirect_to "/"
+  else
+    render :new
+  end
+end
+end
